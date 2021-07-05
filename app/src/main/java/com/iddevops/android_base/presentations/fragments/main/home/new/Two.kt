dce4ae47d84875f91d130c9507c49f541ab778c0
St. Ayudha Junior <stefanus.ayudha@gmail.com>
2021-07-05 09:29:54 +0700

    move app properties to gradle.propertie

---
+++ app/src/main/java/com/iddevops/android_base/presentations/fragments/main/home/new/Two.kt
@@ -0,0 +1,25 @@
+package com.iddevops.android_base.presentations.fragments.main.home.new
+
+import com.iddevops.android_base.databinding.FragmentTwoBinding
+import com.iddevops.android_base.utils.bases.presentation.BaseFragment
+
+class Two : BaseFragment<FragmentTwoBinding>() {
+
+    override fun getLayoutBinding() = FragmentTwoBinding.inflate(layoutInflater)
+
+    override fun initData() {
+
+    }
+
+    override fun initUI() {
+
+    }
+
+    override fun initAction() {
+
+    }
+
+    override fun initObserver() {
+
+    }
+}
---
+++ app/src/main/java/com/iddevops/android_base/presentations/fragments/main/home/three/Three.kt
@@ -0,0 +1,25 @@
+package com.iddevops.android_base.presentations.fragments.main.home.three
+
+import com.iddevops.android_base.databinding.FragmentThreeBinding
+import com.iddevops.android_base.utils.bases.presentation.BaseFragment
+
+class Three : BaseFragment<FragmentThreeBinding>() {
+
+    override fun getLayoutBinding() = FragmentThreeBinding.inflate(layoutInflater)
+
+    override fun initData() {
+
+    }
+
+    override fun initUI() {
+
+    }
+
+    override fun initAction() {
+
+    }
+
+    override fun initObserver() {
+
+    }
+}
